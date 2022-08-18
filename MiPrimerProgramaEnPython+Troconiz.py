189f9e72b908f611970ecae6ed4c6833fc942a45
jesus troconiz <jtroconiz@outlook.es>
2022-08-18 14:47:14 -0300

    first commit

---
+++ MiPrimerProgramaEnPython+Troconiz.py
@@ -0,0 +1,10 @@
+
+estudiante = input("Estudiante, ingrese su nombre:")
+
+nota1 = float(input("Ahora ingrese la primera nota "))
+nota2 = float(input("Continue con la segunda nota "))
+nota3 = float(input("Para finalizar ingrese la tercera nota "))
+
+promedio = (nota1 * 0.2) + (nota2 * 0.3) + (nota3 * 0.5)
+print( estudiante + 'te informo que tu promedio es de:' + " " + str( promedio))
+                    
